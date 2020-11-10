1b35f23d671da541f76ceb51a20189ec40142683
Anton Satorin <brayan.1702@mail.ru>
2020-11-10 07:50:16 +0400

    fix: https

--- store/articles.js
+++ store/articles.js
@@ -15,7 +15,7 @@ export const mutations = {
 
 export const actions = {
   async fetch({ commit }){
-    const url = 'http://newsapi.org/v2/top-headlines?' +
+    const url = 'https://newsapi.org/v2/top-headlines?' +
     'country=us&' +
     'category=technology&' +
     'apiKey=f72198c640cb430295cfe8562bbff1c5';
